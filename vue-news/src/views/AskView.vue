b78b180e262d93784c67d4f3d8594e58a7274a5c
chwangmin <ckm7907@naver.com>
2025-01-16 10:49:29 +0900

    [section 9]

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for = "ask in askItems" v-bind:key="ask.id">{{ ask.title }}</div>
+    <p v-for="item in askItems" v-bind:key="item.id">
+      <a :href="item.url">{{ item.title }}</a>
+      <small>{{item.time_ago}} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <div v-for="user in this.$store.state.news" v-bind:key="user.id">{{ user.title }}</div>
+    <!-- <div v-for="user in this.$store.state.news" v-bind:key="user.id">{{ user.title }}</div> -->
+    <p v-for="item in this.$store.state.news" v-bind:key="item.id">
+      <a href="https://google.com">{{ item.title }}</a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
